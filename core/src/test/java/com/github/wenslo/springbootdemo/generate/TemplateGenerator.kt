86888024841c9139b4a4ea5e2c31a7a87758c4df
wenhailin <564544234@qq.com>
2020-05-03 13:16:30 +0800

    Code Generator added, permission generator removed

--- core/src/test/java/com/github/wenslo/springbootdemo/generate/TemplateGenerator.kt
+++ core/src/test/java/com/github/wenslo/springbootdemo/generate/TemplateGenerator.kt
@@ -4,7 +4,9 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
-var df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+val df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+val nowTimeStr = df.format(LocalDateTime.now());
+val author = "wenhailin"
 val codeStore = "code";
 fun main() {
     val generator = TemplateGenerator()
@@ -22,25 +24,131 @@ class TemplateGenerator {
         generateService(entity)
         println("-----------------开始生成Controller-----------------")
         generateController(entity)
-        println("-----------------开始生成Permission-----------------")
-        generatePermission(entity)
 
-    }
-
-    private fun generatePermission(entity: KClass<*>) {
-        //TODO
     }
 
     private fun generateController(entity: KClass<*>) {
-        //TODO
+        val entityName = entity.simpleName;
+        val fileName = "${entityName}Controller";
+        val text = """
+
+import com.github.wenslo.fluent.core.domain.Response;
+import com.github.wenslo.springbootdemo.controller.BaseController;
+import com.github.wenslo.springbootdemo.model.system.Role;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
+
+
+/**
+ * @author $author
+ * @version 0.0.1
+ * @createTime $nowTimeStr
+ * @description
+ */
+@RestController
+@RequestMapping("$entityName")
+public class $fileName extends BaseController {
+    private static final Logger logger = LoggerFactory.getLogger($fileName.class);
+
+    @Autowired
+    private ${entityName}Service service;
+
+    @RequestMapping("/save")
+    public Response save(@RequestBody $entityName dto) {
+        $entityName save = service.save(dto);
+        return Response.success(save.getId());
     }
 
-    private fun generateCondition(entity: KClass<*>) {
-        //TODO
+
+    @RequestMapping("/queryByPage")
+    public Page<$entityName> queryByPage(@RequestBody ${entityName}Condition condition) {
+        logger.debug("The currently operator is {}, condition is {}", getLoginUsername(), gson.toJson(condition));
+        Page<$entityName> page = service.getByCondition(condition, condition.getPageable());
+        return page;
+    }
+
+
+    @RequestMapping("/remove/{id}")
+    public Response remove(@PathVariable Long id) {
+        logger.debug("be remove user id is {}", id);
+        servie.remove(id);
+        return Response.SUCCESS;
+    }
+
+    @RequestMapping("/detail/{id}")
+    public Response detail(@PathVariable Long id) {
+        $entityName dto = service.get(id);
+        return Response.success(dto);
+    }
+}
+
+        """
+        generateFile(fileName, text)
     }
 
     private fun generateService(entity: KClass<*>) {
-        //TODO
+
+        val entityName = entity.simpleName;
+        val interfaceName = "${entityName}Service";
+        val interfaceText = """
+import com.github.wenslo.fluent.data.service.LongIdService;
+
+/**
+ * @author $author
+ * @version 0.0.1
+ * @createTime $nowTimeStr
+ * @description
+ */
+public interface $interfaceName extends LongIdService<$entityName, ${entityName}Condition> {
+}
+
+
+        """
+        generateFile(interfaceName, interfaceText)
+
+        val implementationName = "${entityName}ServiceImpl";
+        val implementationText = """
+
+import com.github.wenslo.fluent.data.service.impl.LongIdServiceImpl;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author $author
+ * @version 0.0.1
+ * @createTime $nowTimeStr
+ * @description
+ */
+@Service
+@Transactional
+public class $implementationName extends LongIdServiceImpl<${entityName}, ${entityName}Condition> implements ${entityName}Service {
+
+    @Override
+    protected List<Predicate> conditionBuild(${entityName}Condition condition) {
+        List<Predicate> conditionBuilder = super.conditionBuild(condition);
+        return conditionBuilder;
+    }
+}
+
+        """
+        generateFile(implementationName, implementationText)
     }
 
     private fun generateRepository(entity: KClass<*>) {
@@ -50,14 +158,36 @@ class TemplateGenerator {
 import com.github.wenslo.fluent.data.repository.LongIdRepository;
 import org.springframework.stereotype.Repository;
 /**
- * @author wenhailin
+ * @author $author
  * @version 0.0.1
- * @createTime ${df.format(LocalDateTime.now())}
+ * @createTime $nowTimeStr
  * @description
  */
 @Repository
 public interface $fileName extends LongIdRepository<$entityName, Long> {
 }
+        """
+        generateFile(fileName, text)
+    }
+
+    private fun generateCondition(entity: KClass<*>) {
+
+        val entityName = entity.simpleName;
+        val fileName = "${entityName}Condition";
+        val text = """
+
+import com.github.wenslo.fluent.core.condition.PageCondition;
+
+/**
+ * @author $author
+ * @version 0.0.1
+ * @createTime $nowTimeStr
+ * @description
+ */
+public class $fileName extends PageCondition {
+    
+}
+
         """
         generateFile(fileName, text)
     }
